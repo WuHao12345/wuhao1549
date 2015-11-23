0d5b7ac4ac019867fa48d4f5cd1a76ada00d5ced
qianfeng <qianfeng@woliangchen.local>
2015-11-23 15:35:39 +0800

    sd

--- GitWuhao/GitWuhao/ViewController.m
+++ GitWuhao/GitWuhao/ViewController.m
@@ -17,7 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"lmdsb");
+
 }
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
